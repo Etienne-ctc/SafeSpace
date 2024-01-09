6f456cec2276800d8e04716c37e916c92ef6537c
Zeta-12 <j.beloin@orange.fr>
2024-01-09 12:32:51 +0100

    Continue

--- app/src/main/java/com/example/safespace/DayActivitiesPatient.java
+++ app/src/main/java/com/example/safespace/DayActivitiesPatient.java
@@ -3,12 +3,34 @@ package com.example.safespace;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+
+import java.util.HashMap;
 
 public class DayActivitiesPatient extends AppCompatActivity {
+    private CheckBox a_check;
+    private CheckBox b_check;
+    private CheckBox c_check;
+    private CheckBox d_check;
+    private CheckBox e_check;
+    private CheckBox f_check;
+    private HashMap<Integer, CheckBox> hash_checkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day_activities);
+
+        a_check = findViewById(R.id.a_checkBox);
+        b_check = findViewById(R.id.b_checkBox);
+        c_check = findViewById(R.id.c_checkBox);
+        d_check = findViewById(R.id.d_checkBox);
+        e_check = findViewById(R.id.e_checkBox);
+        f_check = findViewById(R.id.f_checkBox);
+
+        String[] letters = new String[]{"a","b","c","d","e","f"};
+        for (int i = 0; i<6; i++){
+            hash_checkBox.put(i, String.valueOf(letters[i]+"_checkBox"));
+        }
     }
 }

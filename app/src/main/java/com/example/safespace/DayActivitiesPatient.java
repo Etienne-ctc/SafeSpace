e53ebd41c73c4942ff58e073b1a987cf90df7fb1
Zeta-12 <j.beloin@orange.fr>
2024-01-09 14:32:20 +0100

    Continue

--- app/src/main/java/com/example/safespace/DayActivitiesPatient.java
+++ app/src/main/java/com/example/safespace/DayActivitiesPatient.java
@@ -2,10 +2,12 @@ package com.example.safespace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
-
-import java.util.HashMap;
+import android.widget.Toast;
 
 public class DayActivitiesPatient extends AppCompatActivity {
     private CheckBox a_check;
@@ -14,7 +16,8 @@ public class DayActivitiesPatient extends AppCompatActivity {
     private CheckBox d_check;
     private CheckBox e_check;
     private CheckBox f_check;
-    private HashMap<Integer, CheckBox> hash_checkBox;
+    private Button retour;
+    private Integer stats = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +31,36 @@ public class DayActivitiesPatient extends AppCompatActivity {
         e_check = findViewById(R.id.e_checkBox);
         f_check = findViewById(R.id.f_checkBox);
 
-        String[] letters = new String[]{"a","b","c","d","e","f"};
-        for (int i = 0; i<6; i++){
-            hash_checkBox.put(i, String.valueOf(letters[i]+"_checkBox"));
-        }
+        retour = findViewById(R.id.retour_button);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        retour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(a_check.isChecked()){
+                    stats++;
+                }if(b_check.isChecked()){
+                    stats++;
+                }if(c_check.isChecked()){
+                    stats++;
+                }if(d_check.isChecked()){
+                    stats++;
+                }if(e_check.isChecked()){
+                    stats++;
+                }if(f_check.isChecked()){
+                    stats++;
+                }
+                Toast.makeText(DayActivitiesPatient.this, String.valueOf(stats), Toast.LENGTH_LONG).show();
+
+                // Envoyer stats to BDD
+                stats = 0;
+
+                Intent home_intent = new Intent(DayActivitiesPatient.this, HomePatient.class);
+                startActivity(home_intent);
+            }
+        });
     }
 }

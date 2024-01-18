6973502b9c08776f6b6666981a591fc2cf7a638a
Zeta-12 <j.beloin@orange.fr>
2024-01-18 16:01:17 +0100

    Add notification system in HomePatient with the 'createNotify' function

--- app/src/main/java/com/example/safespace/HomePatient.java
+++ app/src/main/java/com/example/safespace/HomePatient.java
@@ -1,8 +1,15 @@
 package com.example.safespace;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +28,9 @@ public class HomePatient extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_patient);
+
+        // Notifications Test
+        createNotify();
 
         param = findViewById(R.id.paramètres_button);
         param.setOnClickListener(new View.OnClickListener() {
@@ -84,5 +94,29 @@ public class HomePatient extends AppCompatActivity {
                 startActivity(cal_patient);
             }
         });
+    }
+
+    public void createNotify() {
+        // Intent For onTap notifications
+        Intent intent = new Intent(this, HomePatient.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        // Notifications Channel
+        NotificationChannel channel = new NotificationChannel("Test Notifications", "SafeSpace Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+
+        // My Notifications
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(HomePatient.this, "Test Notifications")
+                .setContentTitle("Notications from HomePatient")
+                .setContentText("This is a notification from HomePatient page")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        // Notify user
+        notificationManager.notify(1, builder.build());
     }
 }
--- app/src/main/java/com/example/safespace/MainActivity.java
+++ app/src/main/java/com/example/safespace/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.safespace;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -46,4 +54,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 }

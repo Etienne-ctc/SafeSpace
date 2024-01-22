e13a3af36566b339245c5340b92cf7f0931fe93f
Zeta-12 <j.beloin@orange.fr>
2024-01-22 14:10:53 +0100

    Add HomePro

--- app/src/main/java/com/example/safespace/HomePro.java
+++ app/src/main/java/com/example/safespace/HomePro.java
@@ -2,13 +2,55 @@ package com.example.safespace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePro extends AppCompatActivity {
+    private Button param;
+    private Button calendrier;
+    private Button patients;
+    private Button qrcode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_pro);
+
+        param = findViewById(R.id.param_button);
+        param.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent param_intent = new Intent(HomePro.this, Parametres.class);
+                startActivity(param_intent);
+            }
+        });
+
+        calendrier = findViewById(R.id.cal_pro_button);
+        calendrier.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cal_pro_intent = new Intent(HomePro.this, CalendrierPro.class);
+                startActivity(cal_pro_intent);
+            }
+        });
+
+        patients = findViewById(R.id.liste_patients_button);
+        patients.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent patients_intent = new Intent(HomePro.this, ListePatientPro.class);
+                startActivity(patients_intent);
+            }
+        });
+
+        qrcode = findViewById(R.id.qr_button);
+        qrcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent qrcode_intent = new Intent(HomePro.this, QRCodePro.class);
+            }
+        });
     }
 }
